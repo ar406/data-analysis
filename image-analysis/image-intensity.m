de2d1a5295dfef4062149f12fc1167b397927e49
ar406 <66774262+ar406@users.noreply.github.com>
2021-08-21 14:05:34 +0200

    Create image-intensity.m

---
+++ image-analysis/image-intensity.m
@@ -0,0 +1,21 @@
+clear;
+clc;
+close all;
+
+filename = input ('Please enter filename: ','s')
+I = imread(filename);
+
+#use histogram to find most common intensity 
+[counts,binLocations] = imhist(I);
+figure,imhist(I);
+[max_value, max_index] = max(counts);
+binLocations(max_index);
+intensity_percent=100*binLocations(max_index)/255;
+
+#find size in pixels
+row_pixels=size(I(:,:,1),1); 
+col_pixels=size(I(:,:,1),2);
+
+#output
+fprintf('Image size: %d * %d pixels \n', row_pixels, col_pixels);
+fprintf('Most common intensity (0%%=black, 100%%=white): %d%% \n',intensity_percent);
